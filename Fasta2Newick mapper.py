ccc6cd918279e2c9cfa37eed3477e169212e5ba8
Paulo <14587420+Hatsura@users.noreply.github.com>
2018-01-22 22:06:44 -0300

    Fixed an error caused by parenthesis in headers
    Parenthesis are characters used by NEWICK format in structuring the tree/phylogram. Better don't mess with it.

--- Fasta2Newick mapper.py
+++ Fasta2Newick mapper.py
@@ -1,11 +1,27 @@
 '''
 	'FASTA header into Newick' Mapper
+	release date: 17-jan-2018
 	by Paulo 
 	GitHub: @Hatsura
 	
-	version: 1.0
-	release date: 17-jan-2018
+	version: 1.1
+	last updated: 23-01-2018
 '''
+# -- CONFIG--
+# This separator character will be used to separate description from specific name.
+# ie.: homeobox protein - Apis melifera
+# IMPORTANT NOTE: you can't use ','
+sep = '@' 
+
+# -- END of CONFIG --
+
+# Aesthetics: adding spaces around separator
+sep = ' '+sep+' '
+
+#Foolproofing
+if sep in [',',':',';',')','(']:
+	sep = ' '
+
 from tkinter import Tk,filedialog #Stuff for file open/saving. -- https://pythonspot.com/en/tk-file-dialogs/
 
 def parse_header(header):
@@ -17,21 +33,26 @@ def parse_header(header):
 	
 	# First, get the organism name
 	temp_header = header.split('[')				# ID desc + Organism name
-	organism_name = temp_header[1].replace(']','').strip()	# Organism name
-	temp_header.remove(temp_header[1]) #Removing from list
+	organism_name = temp_header[1].replace(']','').strip()	# Organism name, removing '\n'
+	# Removing organism name from splitted header's list.
+	# Acession and Description remains.
+	temp_header.remove(temp_header[1]) 
 
-	# Second, get the ID
+	# Second, get the ID.
+	# Separating by spaces, acession will always be the first...in current NCBI format.
 	acession = temp_header[0].split(' ')[0].replace('>','').strip()
 
 	# The remaining text is the description
-	description = temp_header[0].replace(acession,'').replace('>','').strip(' ').replace(':','--')
+	# Cleaning process: removal of acession, removal of surrounding spaces and removing
+	# the NEWICK-illegal chars (':', '(', ')'' )for '--'.
+	description = temp_header[0].replace(acession,'').strip(' ').replace(':','--').replace('(','--').replace(')','--')
 
 	return (acession, description, organism_name)
 
 if __name__ == '__main__':
-	root = Tk() #Janela 'root', 'root de todos os males'.
-	root.withdraw() #Esconde a janela 'root'
-	My_file = filedialog.askopenfilename(title='Selecione o seu arquivo FASTA com os cabeçalhos.',
+	root = Tk() 	# Main window (root)
+	root.withdraw() # Hiding 'root' window
+	My_file = filedialog.askopenfilename(title='Select the FASTA file containing the sequence headers.',
 	                                      filetypes = ( ("FASTA files \(*.fasta\)","*.fasta"),
 	                                                    ("All files","*.*")
 	                                                  )
@@ -40,14 +61,15 @@ if __name__ == '__main__':
 	with open(My_file,'r') as f:
 		headers = f.readlines()
 
-	# Filtrando e deixando apenas os cabeçalhos
-	headers = [x for x in headers if '>' in x]
+	# Filtering and leaving only the headers
+	# Also, removing the '>'
+	headers = [x.replace('>','') for x in headers if '>' in x]
 
 	parsed_headers = []
 	for h in headers:
 		parsed_headers.append(parse_header(h))
 
-	My_file = filedialog.askopenfilename(title='Selecione o seu arquivo NEWICK da árvore.',
+	My_file = filedialog.askopenfilename(title='Select your NEWICK format file.',
 	                                      filetypes = ( ("NEWICK files \(*.nwk\)","*.nwk"),
 	                                                    ("All files","*.*")
 	                                                  )
@@ -61,11 +83,12 @@ if __name__ == '__main__':
 
 	# Remapping IDs -> description [organism name]
 	for k in parsed_headers:
-		name = k[1] + ' ' + k[2]
+		name = k[1] + sep + k[2]
 
 		# In case other protein has same 
 		if name in phylogram:
-			name += '*'
+			# Adding a cumulative '*' for same node proteins.
+			name += '*'	
 		phylogram = phylogram.replace(k[0], name)
 
 	result_file = filedialog.asksaveasfilename( title='Saving converted NEWICK file into...',
@@ -73,6 +96,5 @@ if __name__ == '__main__':
 	                                                filetypes= [('*.nwk','.nwk')]
 												)
 
-	print(phylogram)
 	with open(result_file, 'w') as f3:
 		f3.write(phylogram)
